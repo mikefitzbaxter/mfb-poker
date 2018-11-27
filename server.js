c571ef9310fc61a925a6d920511f87f93899f1d8
Mike Fitzbaxter <mikefitzbaxter@gmail.com>
2018-11-27 11:48:16 -0800

    404 Error handling and added robots.txt

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const compression = require('compression')
 const app = express()
 const port = process.argv[2] || 3000
 
+/* ###### TEMPLATE CONFIG ###### */
 // configure nunjucks template engine
 // http://mozilla.github.io/nunjucks
 nunjucks.configure('templates', {
@@ -13,13 +14,23 @@ nunjucks.configure('templates', {
 	watch: true
 })
 
+/* ###### MIDDLEWARE ###### */
 // static files middleware
 app.use(express.static('dist'))
 // gzip compression on everything
 app.use(compression())
 
-// routing
-app.get( '/', (req, res) => res.render('pages/index.njk') )
+/* ###### ROUTING ###### */
+// Home
+app.get( '/', function(req, res) {
+	res.render('pages/index.njk')
+})
 
+// 404 catchall
+app.get( '*', function(req, res) {
+	res.status(404).render('pages/404.html')
+})
+
+/* ###### SERVER START ###### */
 // start server and listen on port
 app.listen(port, () => console.log(`App running on port ${port}!`))

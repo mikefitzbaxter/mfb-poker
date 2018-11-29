df789ea0ec9d2fd17046283258800ad6c2e96b0b
Mike Fitzbaxter <mikefitzbaxter@gmail.com>
2018-11-28 20:52:49 -0800

    Added mocha and chai test framework and ran first test in /test/test.js

--- test/test.js
+++ test/test.js
@@ -1,2 +1,11 @@
-// test.js
+const assert = require('assert')
+const expect = require('chai').expect
+const should = require('chai').should()
 
+describe('Array', function() {
+ 	describe('#indexOf()', function() {
+		it('should return -1 when the value is not present', () => {
+			assert.equal([1,2,3].indexOf(4), -1)
+		})
+	})
+})

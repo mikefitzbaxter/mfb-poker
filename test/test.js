135c4324f2f5fc6438696bacef350977122ca28e
Mike Fitzbaxter <mikefitzbaxter@gmail.com>
2018-11-28 21:56:49 -0800

    adding a more robust framework to starting the server with /bin/www and a new app.js structure from the MDN tutorials.

--- test/test.js
+++ test/test.js
@@ -1,11 +1,15 @@
 const assert = require('assert')
-const expect = require('chai').expect
-const should = require('chai').should()
+const chai   = require('chai')
+const expect = chai.expect
+const should = chai.should()
+
+// Don't use arrow functions when dealing with Mocha as the `this` keyword doesn't work
 
 describe('Array', function() {
  	describe('#indexOf()', function() {
 		it('should return -1 when the value is not present', () => {
-			assert.equal([1,2,3].indexOf(4), -1)
+			[1, 2, 3].indexOf(4).should.equal(-1)
+			//assert.equal([1,2,3].indexOf(4), -1)
 		})
 	})
 })
